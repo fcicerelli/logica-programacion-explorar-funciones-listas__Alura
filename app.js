526bb47e70abe3b707b0d2b04f48296df3c7b446
fcicerelli <fcicerellitic@gmail.com>
2025-02-09 18:41:57 -0300

    function intentoDeUsuario()

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = "Juego de número secreto";
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = "Indica un número del 1 al 10";
+
+function intentoDeUsuario() {
+  alert('Click desde el botón intentar');
+};
